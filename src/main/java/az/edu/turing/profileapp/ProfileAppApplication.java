ec0b97bd57f19126a29ecd8462f114cb5efadb82
TheCoteen <knanqurbanov22@gmail.com>
2024-07-27 22:02:07 +0400

    adding main logic

--- src/main/java/az/edu/turing/profileapp/ProfileAppApplication.java
+++ src/main/java/az/edu/turing/profileapp/ProfileAppApplication.java
@@ -2,8 +2,10 @@ package az.edu.turing.profileapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProfileAppApplication {
 
     public static void main(String[] args) {
